c556a93c12828ae2843a68be118afd276f35e8c9
jumaboyev xojiakbar <109433027+xojimlengineer@users.noreply.github.com>
2023-11-16 22:25:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from fastai.vision.all import*
 import plotly.express as px
+import platform
 import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath=pathlib.WindowsPath
+plt = platform.system()
+if plt=='Linux' : pathlib.WindowsPath=pathlib.PosixPath
 
 #title
 st.title("Transport klassifikatsiya qiluvchi model ")
